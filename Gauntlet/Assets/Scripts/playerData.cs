1f17170a9dd39b924f63e6f2e034d53c789811cd
Arkhilean <83036432+Arkhilean@users.noreply.github.com>
2021-05-04 13:03:08 -0700

    Added Player Data and Player
    Added player data info

--- Gauntlet/Assets/Scripts/playerData.cs
+++ Gauntlet/Assets/Scripts/playerData.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class playerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Every player needs variables for their stats
+    public float pMS; //Player Movement speed
+    public int pHP; //Player Health Points
+    public int pM; //Player Magic
+    public int pLD; //Player Long-Range Damage
+    public int pLS; //Player Long-Range Speed
+    public int pSD; //Player Short-Range Damage
+    public int pD; //Player Defense
+
+    //Every player needs to track their coordinates
+    public float cPX; //Current Player X value
+    public float cPY; //Current Player Y value
+
+    //Can short range attacks destroy Generators
+    public bool pSE;
+
+    //Variable for checking which player
+    public int cPlayer;
+
+    //Player Movement function
+    public void pMovement()
     {
-        Debug.Log("Testing");
+        //Check which player this is
+        if (cPlayer == 1)
+        {
+            //Check for if player is within bounds and is being told to go up
+            if (Input.GetKey(KeyCode.W))
+            {
+                cPY = cPY + pMS;
+            }
+
+            //Check if player is told to go left within bounds
+            if (Input.GetKey(KeyCode.A))
+            {
+                cPX = cPX - pMS;
+            }
+
+            //Check if player is told to go right within bounds
+            if (Input.GetKey(KeyCode.D))
+            {
+                cPX = cPX + pMS;
+            }
+
+            //Check if player is told to go down within bounds
+            if (Input.GetKey(KeyCode.S))
+            {
+                cPY = cPY - pMS;
+            }
+        }
+
+        else if (cPlayer == 2)
+        {
+            //Check if player is told to go up within bounds
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Player Attack function
 }
